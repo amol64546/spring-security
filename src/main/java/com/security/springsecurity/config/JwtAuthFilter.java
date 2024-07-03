918c4411e026842a4c001fbe632685b689d6647d
Amol Nakhate <amol64546@gmail.com>
2024-07-03 21:38:06 +0530

    enpoint changed

--- src/main/java/com/security/springsecurity/config/JwtAuthFilter.java
+++ src/main/java/com/security/springsecurity/config/JwtAuthFilter.java
@@ -2,9 +2,12 @@ package com.security.springsecurity.config;
 
 import com.security.springsecurity.JwtHelper;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthFilter extends OncePerRequestFilter {
 
   private final UserDetailsService userDetailsService;
@@ -22,40 +26,37 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-    FilterChain filterChain) {
-    try {
-      String authHeader = request.getHeader("Authorization");
+    FilterChain filterChain) throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
 
-      String token = null;
-      String username = null;
-      if (authHeader != null && authHeader.startsWith("Bearer ")) {
-        token = authHeader.substring(7);
-        username = JwtHelper.extractUsername(token);
-      }
+    String token = null;
+    String username = null;
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      token = authHeader.substring(7);
+      username = JwtHelper.extractUsername(token);
+    }
 
 //      If the accessToken is null. It will pass the request to next filter in the chain.
 //      Any login and signup requests will not have jwt token in their header, therefore they will be passed to next filter chain.
-      if (token == null) {
-        filterChain.doFilter(request, response);
-        return;
-      }
+    if (token == null) {
+      filterChain.doFilter(request, response);
+      return;
+    }
 
 //       If any accessToken is present, then it will validate the token and then authenticate the request in security context
-      if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if (JwtHelper.validateToken(token, userDetails)) {
-          UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-            userDetails, null, null);
-          authenticationToken.setDetails(
-            new WebAuthenticationDetailsSource().buildDetails(request));
-          SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        }
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+      if (JwtHelper.validateToken(token, userDetails)) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+          userDetails, null, null);
+        authenticationToken.setDetails(
+          new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
       }
-
-      filterChain.doFilter(request, response);
-    } catch (Exception e) {
-      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
     }
+
+    filterChain.doFilter(request, response);
+
   }
 
 }
--- src/main/java/com/security/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurity/config/SecurityConfig.java
@@ -3,14 +3,11 @@ package com.security.springsecurity.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,20 +24,18 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
-        .cors(AbstractHttpConfigurer::disable)
-        .csrf(AbstractHttpConfigurer::disable)
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(HttpMethod.POST, "/signup/**").permitAll()
-            .requestMatchers(HttpMethod.POST, "/login/**").permitAll()
-            .requestMatchers(HttpMethod.GET, "/swagger/**").permitAll()
-            .anyRequest().authenticated())
+      .cors(AbstractHttpConfigurer::disable)
+      .csrf(AbstractHttpConfigurer::disable)
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/auth/**", "/swagger/**").permitAll()
+        .anyRequest().authenticated())
       .authenticationProvider(authenticationProvider)
       .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
       .logout(logout -> logout
-        .logoutUrl("/v1/users/logout")
+        .logoutUrl("/logout")
         .invalidateHttpSession(true)
-        .logoutSuccessUrl("/v1/users/login")
+        .logoutSuccessUrl("/login")
         .deleteCookies("JSESSIONID"))
       .build();
   }
